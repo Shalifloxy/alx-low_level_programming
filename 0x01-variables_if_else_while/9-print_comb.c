a12fb110a1e9beb12332aa5a39b02770cb3f08e8
Shalifloxy <agbaicynthia46@gmail.com>
2023-08-11 17:14:15 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,23 @@
  * Return - Always 0 (Success)
  */
 int main(void)
+
 {
 int number;
 
 for (number = 0; number <= 9; number++)
 {
-putchar(number + '0');
-if (number < 9)
-{
+putchar(number);
+ 
+if (number == 9)
+  {
+    break;
+  }
+
 putchar(',');
 putchar(' ');
 }
-}
+ 
 putchar('\n');
 
 return (0);
